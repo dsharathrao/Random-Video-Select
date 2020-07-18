e2aee4b0f8e33b84c9a8cceac168fcf1ae14fa07
D Sharath Kumar Rao <50975866+dsharathrao@users.noreply.github.com>
2020-07-18 15:15:07 +0530

    Done validation

--- video.py
+++ video.py
@@ -4,14 +4,13 @@ import random
 FOLDERPATH = "D:\\Tutorials\\Algoexpert\\Become an Algorithms Expert Updated\\"
 RANDOM_VIDEO = random.choice(os.listdir(FOLDERPATH))
 EXTENSION = os.path.splitext(RANDOM_VIDEO)[1]
-EXTENSION1 = os.path.splitext(RANDOM_VIDEO)[0]
-print(EXTENSION1)
-print(EXTENSION)
-print(RANDOM_VIDEO)
 VIDEOEXTENSION = ".mp4"
 if EXTENSION == VIDEOEXTENSION:	pass
 else: RANDOM_VIDEO = random.choice(os.listdir(FOLDERPATH))
 FULLPATH = FOLDERPATH  + RANDOM_VIDEO
 FULLPATH = str(FULLPATH)
+
+print("Video name :" , RANDOM_VIDEO)
+
 print("final : ", FULLPATH)
 
